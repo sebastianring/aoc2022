4b6e5d42208c91dea1121fba0cf8b0495abaed24
Sebastian Ring <sebastian.ring@bring.com>
2024-12-11 08:29:06 +0100

    day 7 part one and two done

--- 2024/seven/seven.go
+++ 2024/seven/seven.go
@@ -2,6 +2,7 @@ package seven
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -56,66 +57,218 @@ func PartOne(filename string) (int, error) {
 	}
 
 	for _, c := range calcs {
-		c.findSolution()
+		fmt.Println("------")
+		fmt.Println(c)
+		sum += c.findSolution()
 	}
 
 	return sum, nil
 }
 
-// 10 23 9
-// 10 + 23 + 9
-// 10 * 23 + 9
-// 10 * 23 * 9
-// 10 + 23 * 9
-//
-// 10 23 9 12
-// 10 + 23 + 9 + 12
-// 10 + 23 + 9 * 12
-// 10 + 23 * 9 * 12
-// 10 * 23 * 9 * 12
-// 10 * 23 * 9 + 12
-// 10 * 23 + 9 + 12
-// 10 + 23 * 9 + 12
-
-type operation func(...int) int
-
 func (c *calc) findSolution() int {
-	operations := []operation{multiply, addition}
+	operations := []string{"*", "+"}
 	placements := len(c.values) - 1
-	_ = operations
 
-	a := "+"
-	b := "*"
-	_ = a
-	_ = b
+	permutations := [][]string{}
 
-	for i := 0; i < placements; i++ {
-		for j := 0; j < placements; j++ {
+	var generate func(current []string)
+	generate = func(current []string) {
+		if len(current) == placements {
+			permutations = append(permutations, append([]string{}, current...))
+			return
+		}
 
+		for _, o := range operations {
+			current = append(current, o)
+			generate(current)
+			current = current[:len(current)-1]
+		}
+	}
+
+	generate([]string{})
+
+	for _, p := range permutations {
+		fmt.Printf("perm: %s\n", p)
+		result := calcResult(c.values, p)
+		if result == c.sum {
+			fmt.Printf(" ----- FOUND CORRECT!!! ------\n")
+			fmt.Printf("result from calcResult: %d\n", result)
+			return result
+		} else {
+			fmt.Printf(" ----- no corr!!! ------\n")
 		}
 	}
 
 	return 0
 }
 
-func recursive(input []int) {
+func calcResult(values []int, p []string) int {
+	fmt.Printf("calc result input, values: %v, p: %v\n", values, p)
+	// allNumbers := []int{}
 
-}
+	prevVal := values[0]
+	curValue := 0
+	for i := 1; i < len(values); i++ {
+		operator := p[i-1]
+		if operator == "*" {
+			curValue = prevVal * values[i]
+		} else {
+			// allNumbers = append(allNumbers, prevVal)
+			curValue = prevVal + values[i]
+		}
 
-func multiply(v ...int) int {
-	result := 0
-	for _, val := range v {
-		result *= val
+		prevVal = curValue
 	}
 
-	return result
+	return curValue
+
+	// allNumbers = append(allNumbers, prevVal)
+
+	// return addition(allNumbers...)
 }
 
-func addition(v ...int) int {
-	result := 0
-	for _, val := range v {
-		result += val
+// func multiply(v ...int) int {
+// 	result := v[0]
+//
+// 	for i := 1; i < len(v); i++ {
+// 		result *= v[i]
+// 	}
+//
+// 	fmt.Printf("multiplied: %v result: %d\n", v, result)
+//
+// 	return result
+// }
+//
+// func addition(v ...int) int {
+// 	result := 0
+// 	for _, val := range v {
+// 		result += val
+// 	}
+//
+// 	fmt.Printf("added: %v result: %d\n", v, result)
+// 	return result
+// }
+//
+
+func PartTwo(filename string) (int, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return 0, err
 	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	sum := 0
+	calcs := []calc{}
+
+	for {
+		lineString, err := reader.ReadString('\n')
+
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+
+			return 0, err
+		}
+
+		lineString = strings.TrimSuffix(lineString, "\n")
+
+		newCalc := calc{}
+		parts := strings.Split(lineString, ":")
+		sumStr := parts[0]
+
+		newCalc.sum, _ = strconv.Atoi(sumStr)
+
+		parts[1] = strings.TrimPrefix(parts[1], " ")
+		parts = strings.Split(parts[1], " ")
+
+		for _, value := range parts {
+			newVal, _ := strconv.Atoi(value)
+			newCalc.values = append(newCalc.values, newVal)
+		}
+
+		calcs = append(calcs, newCalc)
+	}
+
+	for _, c := range calcs {
+		// fmt.Println("------")
+		// fmt.Println(c)
+		sum += c.findSolutionPartTwo()
+	}
+
+	return sum, nil
+}
+
+func (c *calc) findSolutionPartTwo() int {
+	operations := []string{"*", "+", "||"}
+	placements := len(c.values) - 1
+
+	permutations := [][]string{}
+
+	var generate func(current []string)
+	generate = func(current []string) {
+		if len(current) == placements {
+			permutations = append(permutations, append([]string{}, current...))
+			return
+		}
+
+		for _, o := range operations {
+			current = append(current, o)
+			generate(current)
+			current = current[:len(current)-1]
+		}
+	}
+
+	generate([]string{})
+
+	for _, p := range permutations {
+		// fmt.Printf("perm: %s\n", p)
+		result := calcResultPartTwo(c.values, p)
+		if result == c.sum {
+			// fmt.Printf(" ----- FOUND CORRECT!!! ------\n")
+			// fmt.Printf("result from calcResult: %d\n", result)
+			return result
+		} else {
+			// fmt.Printf(" ----- no corr!!! ------\n")
+		}
+	}
+
+	return 0
+}
+
+func calcResultPartTwo(values []int, p []string) int {
+	// fmt.Printf("calc result input, values: %v, p: %v\n", values, p)
+	// allNumbers := []int{}
+
+	prevVal := values[0]
+	curValue := 0
+	for i := 1; i < len(values); i++ {
+		operator := p[i-1]
+		if operator == "*" {
+			curValue = prevVal * values[i]
+		} else if operator == "||" {
+			curValue = combine(prevVal, values[i])
+		} else {
+			// allNumbers = append(allNumbers, prevVal)
+			curValue = prevVal + values[i]
+		}
+
+		prevVal = curValue
+	}
+
+	return curValue
+
+	// allNumbers = append(allNumbers, prevVal)
+
+	// return addition(allNumbers...)
+}
+
+func combine(a, b int) int {
+	c := strconv.Itoa(a) + strconv.Itoa(b)
+	result, _ := strconv.Atoi(c)
 
 	return result
 }
--- 2024/seven/seven_test.go
+++ 2024/seven/seven_test.go
@@ -1,8 +1,10 @@
 package seven
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestDayOne(t *testing.T) {
+func TestPartOne(t *testing.T) {
 	type args struct {
 		filename string
 	}
@@ -17,6 +19,14 @@ func TestDayOne(t *testing.T) {
 			args: args{
 				filename: "data_example.txt",
 			},
+			want:    3749,
+			wantErr: false,
+		},
+		{
+			name: "real",
+			args: args{
+				filename: "data.txt",
+			},
 			want:    -1,
 			wantErr: false,
 		},
@@ -25,11 +35,52 @@ func TestDayOne(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := PartOne(tt.args.filename)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("DayOne() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("PartOne() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("DayOne() = %v, want %v", got, tt.want)
+				t.Errorf("PartOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "example",
+			args: args{
+				filename: "data_example.txt",
+			},
+			want:    11387,
+			wantErr: false,
+		},
+		{
+			name: "real",
+			args: args{
+				filename: "data.txt",
+			},
+			want:    11387,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := PartTwo(tt.args.filename)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PartTwo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("PartTwo() = %v, want %v", got, tt.want)
 			}
 		})
 	}
