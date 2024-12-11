352be76d32def8b7fefc56c613a51960893588cf
Sebastian Ring <sebastian.ring@bring.com>
2024-12-11 15:11:35 +0100

    day 9 part one and two solved

--- 2024/08/eight.go
+++ 2024/08/eight.go
@@ -12,7 +12,7 @@ type pos struct {
 	y int
 }
 
-func DayEight(filename string) (int, error) {
+func DayEightPartOne(filename string) (int, error) {
 	file, err := os.Open(filename)
 
 	if err != nil {
@@ -104,6 +104,78 @@ func createAntinodes(a, b pos, board [][]string) int {
 	return result
 }
 
+func createAntinodesGrid(a, b pos, board [][]string) int {
+	result := 0
+	xDiff, yDiff := getDiff(a, b)
+	fmt.Printf("xdiff: %d, ydiff: %d\n", xDiff, yDiff)
+
+	i := 0
+	for {
+		xOffset := xDiff * i
+		yOffset := yDiff * i
+
+		x := a.x + xOffset
+		y := a.y + yOffset
+
+		fmt.Printf("x: %d, y: %d\n", x, y)
+
+		if outOfBounds(board, x, y) {
+			break
+		}
+
+		if board[y][x] == "#" {
+			i++
+			continue
+		}
+
+		result++
+		board[y][x] = "#"
+		i++
+	}
+
+	i = 0
+	for {
+		xOffset := (xDiff * -1) * i
+		yOffset := (yDiff * -1) * i
+
+		x := a.x + xOffset
+		y := a.y + yOffset
+
+		fmt.Printf("x: %d, y: %d\n", x, y)
+
+		if outOfBounds(board, x, y) {
+			break
+		}
+
+		if board[y][x] == "#" {
+			i++
+			continue
+		}
+
+		result++
+		board[y][x] = "#"
+		i++
+	}
+
+	//
+	// antinode1 := pos{x: a.x + xDiff, y: a.y + yDiff}
+	// antinode2 := pos{x: b.x + xDiff*-1, y: b.y + yDiff*-1}
+	//
+	// if !outOfBounds(board, antinode1.x, antinode1.y) && board[antinode1.y][antinode1.x] != "#" {
+	// 	result++
+	// 	board[antinode1.y][antinode1.x] = "#"
+	// }
+	//
+	// if !outOfBounds(board, antinode2.x, antinode2.y) && board[antinode2.y][antinode2.x] != "#" {
+	// 	result++
+	// 	board[antinode2.y][antinode2.x] = "#"
+	// }
+
+	printBoard(board)
+
+	return result
+}
+
 func getDiff(a, b pos) (int, int) {
 	x := a.x - b.x
 	y := a.y - b.y
@@ -136,4 +208,73 @@ func outOfBounds(board [][]string, x, y int) bool {
 	}
 
 	return false
+}
+
+func DayEightPartTwo(filename string) (int, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	sum := 0
+	board := [][]string{}
+	antennas := map[string][]pos{}
+
+	for {
+		lineString, err := reader.ReadString('\n')
+
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+
+			return 0, err
+		}
+
+		lineString = strings.TrimSuffix(lineString, "\n")
+		currentLine := []string{}
+		for _, char := range lineString {
+			currentLine = append(currentLine, string(char))
+		}
+
+		board = append(board, currentLine)
+	}
+
+	cleanBoard := make([][]string, len(board))
+
+	for y := 0; y < len(board); y++ {
+		rowCopy := make([]string, len(board[y]))
+		copy(rowCopy, board[y])
+
+		for x := 0; x < len(board[y]); x++ {
+			if board[y][x] != "." {
+				rowCopy[x] = "."
+				antennas[board[y][x]] = append(antennas[board[y][x]], pos{x: x, y: y})
+			}
+		}
+
+		cleanBoard[y] = rowCopy
+	}
+
+	for a, pos := range antennas {
+		// fmt.Println(a, pos)
+		// superCleanBoard := copyBoard(cleanBoard)
+
+		for i := 0; i < len(pos)-1; i++ {
+			for j := i + 1; j < len(pos); j++ {
+				fmt.Printf("antenna: %s - checking pos: %v and %d\n", a, pos[i], pos[j])
+				sum += createAntinodesGrid(pos[i], pos[j], cleanBoard)
+			}
+		}
+	}
+
+	fmt.Println("-------")
+	printBoard(board)
+	fmt.Println("-------")
+
+	return sum, nil
 }
--- 2024/08/eight_test.go
+++ 2024/08/eight_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestDayOne(t *testing.T) {
+func TestDayEightPartOne(t *testing.T) {
 	type args struct {
 		filename string
 	}
@@ -59,14 +59,32 @@ func TestDayOne(t *testing.T) {
 			args: args{
 				filename: "data.txt",
 			},
-			want:     -1,
+			want:     259,
 			wantErr:  false,
 			expected: ``,
+		},
+		{
+			name: "example4",
+			args: args{
+				filename: "data_example4.txt",
+			},
+			want:    259,
+			wantErr: false,
+			expected: `T....#....
+...T......
+.T....#...
+.........#
+..#.......
+..........
+...#......
+..........
+....#.....
+..........`,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := DayEight(tt.args.filename)
+			got, err := DayEightPartOne(tt.args.filename)
 			fmt.Println(tt.expected)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("DayOne() error = %v, wantErr %v", err, tt.wantErr)
@@ -74,6 +92,80 @@ func TestDayOne(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("DayOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDayEightPartTwo(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		want     int
+		wantErr  bool
+		expected string
+	}{
+		{
+			name: "example4",
+			args: args{
+				filename: "data_example4.txt",
+			},
+			want:    9,
+			wantErr: false,
+			expected: `T....#....
+...T......
+.T....#...
+.........#
+..#.......
+..........
+...#......
+..........
+....#.....
+..........`,
+		},
+		{
+			name: "example",
+			args: args{
+				filename: "data_example.txt",
+			},
+			want:    34,
+			wantErr: false,
+			expected: `......#....#
+...#....0...
+....#0....#.
+..#....0....
+....0....#..
+.#....A.....
+...#........
+#......#....
+........A...
+.........A..
+..........#.
+..........#.`,
+		},
+		{
+			name: "real",
+			args: args{
+				filename: "data.txt",
+			},
+			want:     34,
+			wantErr:  false,
+			expected: ``,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := DayEightPartTwo(tt.args.filename)
+			fmt.Println(tt.expected)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DayEightPartTwo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("DayEightPartTwo() = %v, want %v", got, tt.want)
 			}
 		})
 	}
