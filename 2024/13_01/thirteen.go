ed4ef5f0cf1f28814e6748b6561faac47a5f437a
Sebastian Ring <sebastian.ring@bring.com>
2025-01-29 20:30:30 +0100

    start day 13

---
+++ 2024/13_01/thirteen.go
@@ -0,0 +1,188 @@
+package thirteen
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type scenario struct {
+	buttonA pos
+	buttonB pos
+	prize   pos
+}
+
+type pos struct {
+	x int
+	y int
+}
+
+func Thirteen(filename string) (int, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	sum := 0
+	i := 0
+	scenarios := []*scenario{}
+	var currentScenario = &scenario{}
+
+	for {
+		lineString, err := reader.ReadString('\n')
+
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+
+			return 0, err
+		}
+
+		lineString = strings.TrimSuffix(lineString, "\n")
+		parts := strings.Split(lineString, ":")
+		// for ctr, part := range parts {
+		// 	fmt.Printf("ctr #%d: %v\n", ctr, part)
+		// }
+
+		var p pos
+
+		if len(parts) > 1 {
+			if strings.Contains("Prize", parts[0]) {
+				xy := strings.Split(parts[1], ",")
+				xstr := strings.TrimPrefix(xy[0], " X=")
+				ystr := strings.TrimPrefix(xy[1], " Y=")
+
+				x, _ := strconv.Atoi(xstr)
+				y, _ := strconv.Atoi(ystr)
+
+				p.x = x
+				p.y = y
+			} else {
+				xy := strings.Split(parts[1], ",")
+				xstr := strings.TrimPrefix(xy[0], " X+")
+				ystr := strings.TrimPrefix(xy[1], " Y+")
+
+				x, _ := strconv.Atoi(xstr)
+				y, _ := strconv.Atoi(ystr)
+
+				p.x = x
+				p.y = y
+			}
+		}
+
+		switch parts[0] {
+		case "Button A":
+			currentScenario.buttonA = p
+		case "Button B":
+			currentScenario.buttonB = p
+		case "Prize":
+			currentScenario.prize = p
+		}
+
+		if strings.Contains("\n", lineString) {
+			scenarios = append(scenarios, currentScenario)
+			currentScenario = &scenario{}
+			i = 0
+		} else {
+			i++
+		}
+	}
+
+	for _, scenario := range scenarios {
+		fmt.Println("--------")
+		fmt.Println(scenario)
+		// fmt.Println("button a")
+		// fmt.Println(scenario.prize.x % scenario.buttonA.x)
+		// fmt.Println(scenario.prize.y % scenario.buttonA.y)
+		// fmt.Println("button b")
+		// fmt.Println(scenario.prize.x % scenario.buttonB.x)
+		// fmt.Println(scenario.prize.y % scenario.buttonB.y)
+
+		// res := permutate(scenario)
+		// fmt.Println("got result: ", res)
+
+		converge, f := scenario.converge()
+		fmt.Println(converge, f)
+		fmt.Println("--------")
+	}
+
+	return sum, nil
+}
+
+func permutate(s *scenario) int {
+	if s == nil {
+		fmt.Printf("nil fail\n")
+		return 0
+	}
+
+	x := 0
+	y := 0
+
+	btns := sortedButtons(s.buttonA, s.buttonB)
+
+	buttonA := 0
+	buttonB := 0
+
+	for x < s.prize.x && y < s.prize.y {
+		leftX := s.prize.x - x
+		leftY := s.prize.y - y
+
+		if leftX%btns[1].x == 0 && leftY%btns[1].y == 0 && leftX/btns[1].x == leftY/btns[1].y {
+			multiplier := leftX / btns[1].x
+			x += btns[1].x * multiplier
+			y += btns[1].y * multiplier
+			buttonB += leftX / btns[1].x
+		} else {
+			x += btns[0].x
+			y += btns[0].y
+			buttonA += 1
+		}
+	}
+
+	return buttonA*3 + buttonB
+}
+
+func sortedButtons(a, b pos) []pos {
+	l := []pos{a, b}
+	slices.SortFunc(l, func(a, b pos) int {
+		one := a.x + a.y
+		two := b.x + b.y
+		if one > two {
+			return 1
+		} else if one == two {
+			return 0
+		}
+
+		return -1
+	})
+
+	return l
+}
+
+func (s *scenario) converge() (bool, int) {
+	x := 0
+	y := 0
+	i := 0
+
+	for x < s.prize.x && y < s.prize.y {
+		x += s.buttonB.x
+		y += s.buttonB.y
+
+		if x%s.buttonA.x == 0 && y%s.buttonA.y == 0 {
+			fmt.Printf("convergence found at %d %d %d\n", x, y, i)
+			return true, 0
+		}
+
+		i++
+	}
+
+	return false, 0
+}
---
+++ 2024/13_01/thirteen_test.go
@@ -0,0 +1,36 @@
+package thirteen
+
+import "testing"
+
+func TestDayOne(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "example",
+			args: args{
+				filename: "data_example.txt",
+			},
+			want:    -1,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Thirteen(tt.args.filename)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("DayOne() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("DayOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
